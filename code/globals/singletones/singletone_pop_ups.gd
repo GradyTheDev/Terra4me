32c8d66f8fbfe39c3eac610d6eea11288dc16d05
wovri <adammisiak2@gmail.com>
2023-09-09 18:33:33 +0200

    Make basic Menu structure + some features 🧱✨ Create main menu 🚀 Add singletones GlobalPopUps + GlobalSounds 🔧 Add button scene with sounds ✨
    Add credits, settings menus ✨
    
    Small infrastructure changes 🏗️
    There is erron with Simple license on web but seems harmless 🥅

---
+++ code/globals/singletones/singletone_pop_ups.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+#SingletoneScenes
+
+enum SCENE {SETTINGS, CREDITS, PAUSE}
+var settings_tscn: PackedScene = preload("res://code/menus/settings/settings.tscn")
+var credits_tscn: PackedScene = preload("res://code/menus/credits/credits.tscn")
+var pause_tscn: PackedScene = preload("res://code/menus/pause/pause.tscn")
+
+var scenes_dict = {
+	SCENE.SETTINGS: settings_tscn.instantiate(),
+	SCENE.CREDITS: credits_tscn.instantiate(),
+	SCENE.PAUSE: pause_tscn.instantiate(),
+}
+
+func _init():
+	assert(SCENE.size() == scenes_dict.size(), "PopUp scenes aren't equal")
+
+
+func call_scene(scene: SCENE):
+	var scene_inst = scenes_dict[scene]
+	if scene_inst.is_inside_tree():
+		self.move_child(scene_inst, -1)
+	else:
+		self.add_child(scenes_dict[scene])
