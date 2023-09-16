a15d513d49e49f669f98757fc8539361d44e5e40
wovri <adammisiak2@gmail.com>
2023-09-16 12:28:49 +0200

    Small tooltip adjustment

--- code/globals/singletones/global_popup_space.gd
+++ code/globals/singletones/global_popup_space.gd
@@ -15,6 +15,17 @@ var scenes_dict = {
 }
 
 
+# This is to remove background for Tooltips
+func _enter_tree() -> void:
+	get_tree().node_added.connect(on_node_added)
+
+func on_node_added(node: Node) -> void:
+	var pp := node as PopupPanel
+	if pp and pp.theme_type_variation == "TooltipPanel":
+		pp.transparent_bg = true
+
+
+
 func _init():
 	assert(POP_UP.size() == scenes_dict.size(), "PopUp scenes aren't equal")
 	for key in scenes_dict:
