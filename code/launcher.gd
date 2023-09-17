d28679e106fe2a44f7d1b7aeb96dfc8173639307
GradyTheDev <50427197+GradyTheDev@users.noreply.github.com>
2023-09-17 12:47:31 -0500

    Added Jam images to splash screen Remove splash screen Misc

--- code/launcher.gd
+++ code/launcher.gd
@@ -1,5 +1,5 @@
 extends CanvasLayer
 
 func _ready():
-	await get_tree().create_timer(2).timeout
+	await get_tree().create_timer(5).timeout
 	get_tree().change_scene_to_file("res://code/menus/main/main_menu.tscn")
