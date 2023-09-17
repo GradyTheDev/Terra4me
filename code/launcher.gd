741f33c551dfe6d149981f796d6a0d848329d5e7
GradyTheDev <50427197+GradyTheDev@users.noreply.github.com>
2023-09-17 12:58:53 -0500

    Changed splash screen Added backgrounds to popup menus

--- code/launcher.gd
+++ code/launcher.gd
@@ -1,5 +1,10 @@
 extends CanvasLayer
 
-func _ready():
-	await get_tree().create_timer(5).timeout
+func _input(event):
+	if event is InputEventKey:
+		if not event.is_pressed():
+			if event.keycode != KEY_F11:
+				_on_timer_timeout()
+
+func _on_timer_timeout():
 	get_tree().change_scene_to_file("res://code/menus/main/main_menu.tscn")
