c296ec9414846c1739a85b71cc7921129d281d16
wovri <adammisiak2@gmail.com>
2023-09-17 14:51:59 +0200

    Make one dialoge box for 4th level + bugfix tween

--- code/GUI/Map.gd
+++ code/GUI/Map.gd
@@ -1,6 +1,6 @@
 extends TextureRect
 
 func _ready():
-	var tween: Tween = get_tree().create_tween().set_loops().set_trans(Tween.TRANS_SINE)
+	var tween = self.create_tween().set_loops().set_trans(Tween.TRANS_SINE)
 	tween.tween_property(self, "rotation", deg_to_rad(10), 5)
 	tween.tween_property(self, "rotation", deg_to_rad(0), 5)
